e5102f3235323aca380b0b1ab132cb7ee0b2cd4c
MMAbbadi <mmabbadi99@gmail.com>
2025-03-09 09:31:21 -0300

    correcao estilizacao

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -21,9 +21,15 @@ const [openModal, setOpenModal] = useState(false)
             
             {/*Corpo Pagina*/}
             <div className={classes.main}>
-                
                 <section className={classes.sobreLoja}>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel magni obcaecati dolor, harum magnam ad, facere dolore at facilis perspiciatis, esse iusto modi sequi corrupti distinctio saepe culpa! Itaque, aliquam?</p>
+                    <div className={classes.nomeFuncionário}>
+                        <h1>Evaldir Ribeiro</h1>
+                    </div>
+                    <p>Evaldir é nosso funcionario mais antigo. <br />
+                    Especialista em corte de cabelo, adulto e infantil.
+                    Design e corte de barba. <br />
+                    Agende um horario com Evaldir Ribeiro e tenha veja o melhor que podemos lhe oferecer
+                    </p>
                     <h4>Tipo de atendimento: Seg.  a Sex. das </h4>
                     <h4>Horarios: das 09:00 as 20:00 </h4>
                 </section>
